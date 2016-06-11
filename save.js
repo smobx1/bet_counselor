e90b9b902ec122182f4c1f2cdf6974d5452cce17
smobx1 <mnmoldoveanu@gmail.com>
2016-06-11 22:44:07 +0300

    v2

---
+++ save.js
@@ -0,0 +1,155 @@
+
+  if (csgl1_test.value >= 0 && csgl1_test.value <= 200) {
+    if(csgl1_test.value >= 0 && csgl1_test.value <= 1.05){
+      csgl2_test.value = 10.00;
+      csgl3_test.value = 30.00;
+
+      csgl1.value = 99;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.05 && csgl1_test.value <= 1.10){
+      csgl2_test.value = 8.00;
+      csgl3_test.value = 21.00;
+
+      csgl1.value = 93;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.10 && csgl1_test.value <= 1.20){
+      csgl2_test.value = 7.00;
+      csgl3_test.value = 19.00;
+
+      csgl1.value = 88;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.20 && csgl1_test.value <= 1.30){
+      csgl2_test.value = 6.00;
+      csgl3_test.value = 12.50;
+
+      csgl1.value = 85;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.30 && csgl1_test.value <= 1.40){
+      csgl2_test.value = 5.50;
+      csgl3_test.value = 7.50;
+
+      csgl1.value = 82;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.40 && csgl1_test.value <= 1.50){
+      csgl2_test.value = 3.65;
+      csgl3_test.value = 6.75;
+
+      csgl1.value = 78;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.50 && csgl1_test.value <= 1.60){
+      csgl2_test.value = 4.33;
+      csgl3_test.value = 6.20;
+
+      csgl1.value = 73;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.60 && csgl1_test.value <= 1.70){
+      csgl2_test.value = 4.20;
+      csgl3_test.value = 5.50;
+
+      csgl1.value = 68;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.70 && csgl1_test.value <= 1.80){
+      csgl2_test.value = 3.80;
+      csgl3_test.value = 4.80;
+
+      csgl1.value = 63;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.80 && csgl1_test.value <= 1.90){
+      csgl2_test.value = 3.75;
+      csgl3_test.value = 4.20;
+
+      csgl1.value = 60;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 1.90 && csgl1_test.value <= 2.00){
+      csgl2_test.value = 3.25;
+      csgl3_test.value = 3.75;
+
+      csgl1.value = 58;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 2.00 && csgl1_test.value <= 2.25){
+      csgl2_test.value = 3.10;
+      csgl3_test.value = 3.50;
+
+      csgl1.value = 53;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 2.25 && csgl1_test.value <= 2.50){
+      csgl2_test.value = 3.20;
+      csgl3_test.value = 2.80;
+
+      csgl1.value = 50;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 2.50 && csgl1_test.value <= 3.00){
+      csgl2_test.value = 3.20;
+      csgl3_test.value = 2.40;
+
+      csgl1.value = 48;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 3.00 && csgl1_test.value <= 3.50){
+      csgl2_test.value = 3.40;
+      csgl3_test.value = 2.75;
+
+      csgl1.value = 45;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 3.50 && csgl1_test.value <= 4.00){
+      csgl2_test.value = 3.25;
+      csgl3_test.value = 2.20;
+
+      csgl1.value = 42;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 4.00 && csgl1_test.value <= 4.50){
+      csgl2_test.value = 3.30;
+      csgl3_test.value = 1.90;
+
+      csgl1.value = 40;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 4.50 && csgl1_test.value <= 5.00){
+      csgl2_test.value = 3.50;
+      csgl3_test.value = 1.70;
+
+      csgl1.value = 38;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 5.00 && csgl1_test.value <= 6.00){
+      csgl2_test.value = 4.80;
+      csgl3_test.value = 1.60;
+
+      csgl1.value = 35;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 6.00 && csgl1_test.value <= 7.00){
+      csgl2_test.value = 4.80;
+      csgl3_test.value = 1.40;
+
+      csgl1.value = 30;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 7.00 && csgl1_test.value <= 9){
+      csgl2_test.value = 4.10;
+      csgl3_test.value = 1.44;
+
+      csgl1.value = 27;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 9 && csgl1_test.value <= 12){
+      csgl2_test.value = 4.25;
+      csgl3_test.value = 1.20;
+
+      csgl1.value = 60;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 12 && csgl1_test.value <= 15){
+      csgl2_test.value = 6.10;
+      csgl3_test.value = 1.14;
+
+      csgl1.value = 25;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 15 && csgl1_test.value <= 20){
+      csgl2_test.value = 8.10;
+      csgl3_test.value = 1.08;
+
+      csgl1.value = 20;
+      csgl2.value = 100-csgl1.value;
+    } else if(csgl1_test.value > 20 && csgl1_test.value <= 30){
+      csgl2_test.value = 11.00;
+      csgl3_test.value = 1.03;
+
+      csgl1.value = 15;
+      csgl2.value = 100-csgl1.value;
+    }
+  }
+}
